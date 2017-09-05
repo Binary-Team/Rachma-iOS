6b4e1cda691db092c262f2fd1efd0af6d4cdaf05
Marwen Doukh <marwen.doukh@gmail.com>
2017-09-05 09:22:04 +0100

    Initial Commit

---
+++ Rachma/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rachma
+//
+//  Created by Marwen Doukh on 9/5/17.
+//  Copyright © 2017 Marwen Doukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
