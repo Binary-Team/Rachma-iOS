400c7173556fa7e00d5347f94f9c0ffec1ccf1bd
Marwen Doukh <marwen.doukh@gmail.com>
2017-09-09 17:46:42 +0100

    Rummy game setting

--- Rachma/RummyGameSetting.swift
+++ Rachma/RummyGameSetting.swift
@@ -11,8 +11,18 @@ import UIKit
 
 class RummyGameSetting: UIViewController {
     
+    var playerNumber = 2
+    var score = 500
+    
+    @IBOutlet weak var playerNumberLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -21,6 +31,41 @@ class RummyGameSetting: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    
+    // change players number
+    @IBAction func increasePlayersNumber(_ sender: Any) {
+        if(playerNumber<4){
+            playerNumber += 1
+            playerNumberLabel.text=String(playerNumber)
+        }
+    }
+    
+    
+    @IBAction func decreasePlayersNumber(_ sender: Any) {
+        if(playerNumber>2){
+            playerNumber -= 1
+            playerNumberLabel.text=String(playerNumber)
+        }
+
+    }
+    
+   
+    @IBAction func increaseScore(_ sender: Any) {
+        score+=50
+        scoreLabel.text=String(score)
+    }
+    
+  
+    
+    @IBAction func decreaseScore(_ sender: Any) {
+        if(score>150){
+            score-=50
+            scoreLabel.text=String(score)
+            
+        }
+
+    }
     
 }
 
