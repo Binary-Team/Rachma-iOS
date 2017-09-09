d2fbaedc6de7a22d2aefa344e980870742c64af3
Marwen Doukh <marwen.doukh@gmail.com>
2017-09-09 09:07:46 +0100

    add Rummy setting VC

---
+++ Rachma/RummyGameSetting.swift
@@ -0,0 +1,26 @@
+//
+//  RummyGameSetting.swift
+//  Rachma
+//
+//  Created by Marwen Doukh on 9/9/17.
+//  Copyright © 2017 Marwen Doukh. All rights reserved.
+//
+
+import UIKit
+
+
+class RummyGameSetting: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
